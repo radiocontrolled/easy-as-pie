1da9acb1021f3524bffe9450c1ff14042927b425
radiocontrolled <radiocontrolled@gmail.com>
2014-09-18 20:35:24 +0100

    adding form validation

--- js/app.js
+++ js/app.js
@@ -1,58 +1,84 @@
-console.log("foo");
+
 (function(){
-	var drawPie = angular.module('EasyAsPie', [])  
-	drawPie.controller('Controller', function(){
-		this.someData = someData;
-	})	
+	var drawPie = angular.module('EasyAsPie', []);
+	
+	drawPie.controller('GoogleAppsKey', ['$scope', function($scope) {
+	     // $scope.text = '1J8RfmzBB6yID1W2shnsqkSAQCiOH_z6B6_l9e5tgaKc';
+	      $scope.text = "";
+	      $scope.submitKey = function(value) {
+	        if ($scope.text) {
+	         console.log(this.text);
+	        }
+	      };
+	    }]);
+
+
 
 	drawPie.directive('pieChart', function(){
 	
-		var w = window.innerWidth/2;
-		var h = window.innerHeight/2;
-		
-		var dataset = someData[0].value;
-		var color = d3.scale.category20c(); 
-		var outerRadius = w / 5;		
-		var svg = d3.select("section")
-			.append("svg")
-			.attr({
-				"width":w,
-				"height":h
-			});
-		
-		//draw wedges
-		var arc = d3.svg.arc()
-			.innerRadius(50) 
-			.outerRadius(outerRadius);
+		function link(scope, element, attr){	
 	
-		// define default pie layout
-		var pie = d3.layout.pie();
+			var foo = scope.data;
+			console.log(foo);
+			
+			
+			var w = window.innerWidth/2;
+			var h = window.innerHeight/2;
+			
+			var dataset = someData[0].value;
+			var color = d3.scale.category20c(); 
+			var outerRadius = w / 5;		
+			var svg = d3.select("section")
+				.append("svg")
+				.attr({
+					"width":w,
+					"height":h
+				});
+			
+			//draw wedges
+			var arc = d3.svg.arc()
+				.innerRadius(50) 
+				.outerRadius(outerRadius);
+		
+			// define default pie layout
+			var pie = d3.layout.pie();
+	
+			var arcs = svg.selectAll("g.arc") 
+				.data(pie(dataset)) 
+				.enter()
+				.append("g")
+				.attr("transform", "translate(" + outerRadius + "," + outerRadius + ")"); 
+				
+			//Draw arc paths
+			arcs.append("path")
+				.attr("d", arc)
+				.attr("fill", function(d) {
+			  		return color(Math.floor(d.value)); 
+				});
+				
+			//Labels
+			arcs.append("text")
+				.attr("transform", function(d) {
+					return "translate(" + arc.centroid(d) + ")";
+				})
+				.text(function(d) {
+					return d.value;
+				});
+		
 
-		var arcs = svg.selectAll("g.arc") 
-			.data(pie(dataset)) 
-			.enter()
-			.append("g")
-			.attr("transform", "translate(" + outerRadius + "," + outerRadius + ")"); 
+			scope.$watch('data', function(data){
 			
-		//Draw arc paths
-		arcs.append("path")
-			.attr("d", arc)
-			.attr("fill", function(d) {
-		  		return color(Math.floor(d.value)); 
-			});
-			
-		//Labels
-		arcs.append("text")
-			.attr("transform", function(d) {
-				return "translate(" + arc.centroid(d) + ")";
-			})
-			.text(function(d) {
-				return d.value;
-			});
-			
-		return{
-			restrict: 'E'
-		};
+			}, true);
+		
+		}
+	
+		return {
+			link: link,
+			restrict: 'A',
+		    scope: {data: "@"}
+        };
+
+	
 	});
 
 	
@@ -64,12 +90,20 @@ console.log("foo");
 		}
 	];
 	
-
+	
+	/*
+	 * test data 
+	 * https://docs.google.com/spreadsheets/d/1J8RfmzBB6yID1W2shnsqkSAQCiOH_z6B6_l9e5tgaKc/pubhtml 
+	 */
+	Tabletop.init( 
+		{ 
+			key: '1J8RfmzBB6yID1W2shnsqkSAQCiOH_z6B6_l9e5tgaKc',
+			callback: function(userData, tabletop) { 
+				//console.log(userData) 
+		},
+		simpleSheet: true });
 			
 })();
-
-/* ad longdesc text */ 
-
 
 
 
